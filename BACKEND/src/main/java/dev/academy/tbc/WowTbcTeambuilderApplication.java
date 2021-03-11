e668ddbfc7af51fbdf96caa688fb5793f1761600
Helge Rannem <helgerannem@gmail.com>
2021-03-11 11:22:06 +0100

    Added start of backend project

---
+++ BACKEND/src/main/java/dev/academy/tbc/WowTbcTeambuilderApplication.java
@@ -0,0 +1,13 @@
+package dev.academy.tbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowTbcTeambuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WowTbcTeambuilderApplication.class, args);
+    }
+
+}
